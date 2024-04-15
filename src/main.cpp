da3b1e5d0433eea095c8a8aab993c5173484435d
henri <henri.ruetzler@gmx.de>
2024-04-15 17:52:24 +0200

    Tastenfeld hinzugefügt

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,14 @@
 #include <LittleFS.h>
 #include <ArduinoJson.h> 
 #include <PubSubClient.h>
+#include <Keypad.h>
+#include <TimeLib.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+// #include <sys/time.h>
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
 
 const char* mqtt_server = "sruetzler.de";
 const int mqtt_port = 8883;
@@ -11,15 +19,38 @@ bool zustandSchloss = true;
 
 const int LED = D8;
 
+struct tm date;
 
-const char* mqtt_user = "henri";
-const char* mqtt_password = "Br@hn*6^ZD*kaK";
+String code = "";
+const byte COLS = 4; //4 Spalten
+const byte ROWS = 4; //4 Zeilen
+
+char hexaKeys[ROWS][COLS]={
+{'1','2','3','A'},
+{'4','5','6','B'},
+{'7','8','9','C'},
+{'*','0','#','D'}
+};
+
+
+byte colPins[COLS] = {D4,D5,D6,D7}; //Definition der Pins für die 3 Spalten
+byte rowPins[ROWS] = {D0,D1,D2,D3}; //Definition der Pins für die 4 Zeilen
+// char Taste;
+
+Keypad Tastenfeld = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
+
+
+
+String mqtt_user;
+String mqtt_password;
 
 JsonDocument daten;
 
 WiFiClientSecure espClient;
 PubSubClient client(espClient);
 
+void checkCode();
+void codeEingabe();
 void callback_mqtt(char* topic, byte* payload, unsigned int length);
 void setup_wifi_mqtt();
 void reconnect_mqtt();
@@ -39,6 +70,10 @@ void setup() {
 
   pinMode(LED, OUTPUT);
 
+  timeClient.begin();
+  timeClient.update();
+
+  setTime(timeClient.getEpochTime());
 
 }
 
@@ -48,6 +83,7 @@ void loop() {
     reconnect_mqtt();
   }
   client.loop();
+  codeEingabe();
 
 
 }
@@ -64,8 +100,8 @@ void setup_wifi_mqtt(){
   String ssid = f.readStringUntil('\n');
   String password = f.readStringUntil('\n');
 
-  // mqtt_user = f.readStringUntil('\n');
-  // mqtt_password = f.readStringUntil('\n');
+  mqtt_user = f.readStringUntil('\n');
+  mqtt_password = f.readStringUntil('\n');
 
 
   f.close();
@@ -104,9 +140,12 @@ void callback_mqtt(char* topic, byte* payload, unsigned int length) {
 
 void reconnect_mqtt() {
   // Loop until we're reconnected
+  String clientId = "ESP8266Client";
+  clientId += String(random(0xffff), HEX);
+
   while (!client.connected()) {
     espClient.setInsecure();
-    if (client.connect("ESP8266Client", mqtt_user, mqtt_password)) {
+    if (client.connect(clientId.c_str(), mqtt_user.c_str(), mqtt_password.c_str())) {
       Serial.println("Connectet");
       // Once connected, publish an announcement...
       client.publish("henri/neuVerbunden", "hello world again");
@@ -166,4 +205,71 @@ void loadJson(){
       digitalWrite(LED, LOW);
     }
 
+}
+
+void codeEingabe(){
+  char Taste = Tastenfeld.getKey();
+  if (Taste)
+  {
+    Serial.println(Taste);
+    if (Taste == 'A')
+    {
+      code = "";
+      Serial.println("Code gelöscht");
+      return;
+    }
+    if (Taste == 'D')
+    {
+      Serial.println("Code überprüfen");
+      Serial.println(code);
+      checkCode();
+      code = "";
+      return;
+    }
+    code += Taste;
+    
   }
+}
+
+
+
+void checkCode(){
+  int len = daten["zeiten"].size();
+
+  for (int i = 0; i < len; i++)
+  {
+      String codeJ = daten["zeiten"][i]["Code"];
+      if (code == codeJ){
+
+        Serial.println("Der Code wurde richtig eingegeben");
+        String datum = daten["zeiten"][i]["Datum"];
+        String uhrzeit = daten["zeiten"][i]["Uhrzeit"];
+
+        long long dateInMillis = daten["zeiten"][i]["DateInMillis"];
+        int stundenInMillis = daten["zeiten"][i]["StundenInMillis"];
+
+        Serial.println(dateInMillis);
+        Serial.println(stundenInMillis);
+        Serial.println(now());
+
+        long long jetzt = now() * 1000;
+
+
+        if (dateInMillis < jetzt && jetzt < dateInMillis + stundenInMillis)
+        {
+          Serial.println("Die Zeit stimmt überein");
+          if (daten["verriegelt"]) {
+            daten["verriegelt"] = false;
+            veraenderung();
+          }
+        } else {
+          Serial.println("Die Zeit stimmt nicht überein");
+        }
+        
+        break;
+      }
+      
+  }
+}
+
+ 
